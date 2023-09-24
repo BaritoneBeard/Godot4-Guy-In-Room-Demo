8f5e8c9f946fbcfd13cc7ccd1984e51351a9d80e
BaritoneBeard <nickalmeder@yahoo.com>
2023-09-23 20:18:41 -0400

    Added gamepad/controller functionality

--- man_in_room_demo.gd
+++ man_in_room_demo.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-
+func _process(delta):
+	if Input.is_action_just_pressed('start'):
+		_on_start_pressed()
+	if Input.is_action_just_pressed('quit'):
+		_on_quit_pressed()
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file('res://world.tscn')
