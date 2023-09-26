f29a27c4716228fea65c809f0bb5c3a9f15be302
BaritoneBeard <nickalmeder@yahoo.com>
2023-09-26 12:50:27 -0400

    Tweaked physics, added double jump, added key rebinds

--- man_in_room_demo.gd
+++ man_in_room_demo.gd
@@ -5,6 +5,8 @@ func _process(delta):
 		_on_start_pressed()
 	if Input.is_action_just_pressed('quit'):
 		_on_quit_pressed()
+	if Input.is_action_just_pressed('dash'):
+		_on_remap_pressed()
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file('res://world.tscn')
@@ -14,3 +16,4 @@ func _on_quit_pressed():
 
 func _on_remap_pressed():
 	get_tree().change_scene_to_file('res://remap.tscn')
+	
